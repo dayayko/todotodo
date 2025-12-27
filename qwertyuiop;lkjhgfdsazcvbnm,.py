e38a27bee82c5aa2fee9f02c822a7021f81e3c43
lida <lidarugh2203@gmail.com>
2025-12-27 13:13:55 +0200

    nifiga

--- qwertyuiop;lkjhgfdsazcvbnm,.py
+++ qwertyuiop;lkjhgfdsazcvbnm,.py
@@ -1,15 +1,2 @@
 
-print("todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo totodo todo todo todo todo todo todo todo todo todo do todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo")
-
-
-
-
-
-
-
-
-
-
-
-
-
+qwertyuioplkjhgfdszcvbnm
